5ade31a949505aa9d8d90d21f7a817ec41623560
José Vinicius <vinicius.j@ifrn.edu.br>
2023-02-17 11:53:05 -0300

    Hello world and variables

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    //New comment
+    /*
+    Multi-line
+    comment
+    */
+    
+    //std = standard
+    std::cout << "I don't know what i'm making" << '\n';
+    std::cout << "But i'll learn" << '\n';
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main(){
+    /*
+    int x; declaration
+    x = 7; assignment
+    */
+    
+
+    /*int x = 7; // declaration + assignment 
+    int y = 2;
+    int sum = x+y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    return 0;
+    */
+
+   //integer (whole number)
+   int age = 19;
+   int year = 2023;
+   int days = 7;
+
+   //double (number including decimal)
+   double price = 7.5;
+   double gpa = 2.5;
+   double temp = 25.1;
+
+    //single character
+    char grade = 'A';
+    char inital = 'J';
+
+    //boolean (true or false)
+    bool student = true;
+    bool power = false;
+
+    //string 
+    std::string name = "José";
+    
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+}
